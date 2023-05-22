1d67f2d55dd9d04f219e575982d88329c2bf72bb
Megan <megan-dlbr@live.fr>
2023-05-22 17:17:36 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
+import App from './components/App';
+import Apropos from './components/Apropos'
+import Error from './components/Error'
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+* {
+    font-family: Montserrat; 
+}
+body {
+  padding-left: 100px; 
+  padding-right: 100px; 
+}
+`
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <GlobalStyle/>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/Apropos" element={<Apropos/>}/>
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
