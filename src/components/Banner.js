75effda6a4682710bbbf99e4a401ca2a9ceff7ed
Megan <megan-dlbr@live.fr>
2023-06-13 15:43:13 +0200

    modif

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,6 +2,7 @@ import '../styles/HomeBanner.css'
 
 
 function HomeBanner(props) {
+    //Un ternaire pour afficher le titre sur l'image de la bannière en lui passant textImage dans l'importation : ou rien 
     return (
     <div className="Banner">
         <h1 className="title-banner">{props.textImage ? props.textImage : ""}</h1>
